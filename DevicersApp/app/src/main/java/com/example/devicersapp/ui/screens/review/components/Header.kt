07d34dbe4c29f2059770016ddb3f6258947465cc
Edwin <eebgaitan6@gmail.com>
2026-08-12 12:22:27 -0500

    Pantalla 09 resena

--- DevicersApp/app/src/main/java/com/example/devicersapp/ui/screens/review/components/Header.kt
+++ DevicersApp/app/src/main/java/com/example/devicersapp/ui/screens/review/components/Header.kt
@@ -1,2 +1,68 @@
 package com.example.devicersapp.ui.screens.review.components
 
+import androidx.compose.foundation.Image
+import com.example.devicersapp.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun EncabezadoResena(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(55.dp)
+            .padding(horizontal = 20.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        // Flecha + título
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Image(
+                painter = painterResource(R.drawable.back_icon),
+                contentDescription = "Volver",
+                modifier = Modifier
+                    .width(24.dp)
+                    .height(24.dp)
+            )
+
+            Text(
+                text = "Reseña",
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(start = 20.dp)
+            )
+        }
+
+        // Guardar
+        Image(
+            painter = painterResource(R.drawable.save),
+            contentDescription = "Guardar",
+            modifier = Modifier
+                .width(24.dp)
+                .height(24.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun EncabezadoResenaPreview() {
+    EncabezadoResena()
+}
--- DevicersApp/app/src/main/java/com/example/devicersapp/ui/screens/review/components/ResponderUsuario.kt
+++ DevicersApp/app/src/main/java/com/example/devicersapp/ui/screens/review/components/ResponderUsuario.kt
@@ -1,2 +1,134 @@
 package com.example.devicersapp.ui.screens.review.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.devicersapp.R
+
+
+//Fondo
+@Composable
+fun Fondo(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(55.dp)
+            .background(
+                color = colorResource(R.color.surface_light)
+            )
+    ) {
+    }
+}
+
+//Caja de Responder a @usuario
+@Composable
+fun Responder(
+    modifier: Modifier = Modifier
+) {
+    OutlinedTextField(
+        value = "Responder a @usuario",
+        onValueChange = {},
+        modifier = modifier,
+        singleLine = true,
+        shape = RoundedCornerShape(15.dp)
+    )
+}
+
+//Boton de enviar
+@Composable
+fun Enviar(
+    modifier: Modifier = Modifier
+){
+    IconButton(
+        onClick = {},
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(R.drawable.enviar_logo),
+            contentDescription = "Enviar",
+            modifier = Modifier
+                .width(28.dp)
+                .height(28.dp)
+        )
+    }
+}
+
+//Mezclar las 3 cosas
+@Composable
+fun ResponderUsuario(
+    modifier: Modifier = Modifier
+){
+    Box(modifier = modifier
+        .fillMaxWidth()
+        .height(55.dp)
+    ){
+    Fondo()
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .align(Alignment.Center)
+            .padding(horizontal = 20.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Responder(
+            modifier = Modifier
+                .width(280.dp)
+                .height(48.dp)
+        )
+
+        Enviar()
+    }
+    }
+}
+
+//Ver fondo
+@Preview
+@Composable
+fun FondoPreview(){
+    Fondo()
+}
+
+//Ver caja de Responder a @usuario
+@Preview(showBackground = true)
+@Composable
+fun ResponderPreview(){
+    Responder()
+}
+
+//Ver icono enviar
+@Preview(showBackground = true)
+@Composable
+fun EnviarPreview(){
+    Enviar()
+}
+
+//Ver ResponderUsuario
+@Preview(
+    showBackground = true
+)
+@Composable
+fun ResponderUsuarioPreview(){
+    ResponderUsuario()
+}
+
